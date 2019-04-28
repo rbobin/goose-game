ef77fa2f308ce7dc7d872c01a87e330e486dacb9
Roman Bobin <roman.bobin@zalando.de>
2019-04-28 14:09:21 +0200

    Implement moving a player

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,6 +1,6 @@
 package example
 
-import example.GameEngine.Player
+import example.GameEngine.{Player, Position}
 
 import scala.collection.immutable.ListMap
 import scala.util.matching.Regex
@@ -17,11 +17,21 @@ object GameEngine {
 
   type Player = String
   type Message = String
+
+  final case class Position(value: Int) extends AnyVal {
+    def asString: String = value match {
+      case 0 => "Start"
+      case v => v.toString
+    }
+  }
+
   val addPlayerRegex: Regex = """add player (\w+)""".r
+  val movePlayerRegex: Regex = """move (\w+) (\d+), (\d+)""".r
 
   def processInput(currentState: GameState, input: () => String): GameState = {
     val state = input() match {
       case addPlayerRegex(name) => addPlayer(name, currentState)
+      case movePlayerRegex(name, r1, r2) => movePlayer(name, r1.toInt, r2.toInt, currentState)
       case _ => currentState.copy(message = "Unrecognized input")
     }
     Console.println(state.message)
@@ -32,10 +42,23 @@ object GameEngine {
     if (state.players.contains(name)) {
       state.copy(message = s"$name: already existing player")
     } else {
-      val players = state.players + (name -> 0)
+      val players = state.players + (name -> Position(0))
       state.copy(players = players, message = s"players: ${players.keySet.mkString(", ")}")
     }
 
+  def movePlayer(name: Player,
+                 diceRollOne: Int,
+                 diceRollTwo: Int,
+                 state: GameState): GameState = {
+    val oldPosition = state.players(name)
+    val newPosition = Position(oldPosition.value + diceRollOne + diceRollTwo)
+    val newMessage = s"$name rolls $diceRollOne, $diceRollTwo. $name moves from ${oldPosition.asString} to " +
+      s"${newPosition.asString}"
+    val newPlayers = state.players + (name -> newPosition)
+    val newState = state.copy(message = newMessage, players = newPlayers)
+    newState
+  }
+
 }
 
-case class GameState(players: ListMap[Player, Int] = ListMap.empty, message: String = "")
+case class GameState(players: ListMap[Player, Position] = ListMap.empty, message: String = "")
--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -1,23 +1,28 @@
 package example
 
+import example.GameEngine.Position
 import org.scalacheck.Prop.BooleanOperators
 import org.specs2.{Specification, _}
 
 import scala.collection.immutable._
 
 class HelloSpec extends Specification with ScalaCheck {
-  def is = s2"""
+  def is =
+    s2"""
 
   "add player" command:
-    adds a player if there is no such name $e1
-    does not add a player if there is such name $e2
+    adds a player if there is no such name $addPlayer1
+    does not add a player if there is such name $addPlayer2
 
+  "move player" command:
+    moves the player $movePlayer1
+    follows specification $movePlayerSpec
   """
 
-  def e1 =
+  private def addPlayer1 =
     prop { (players: Set[String], player: String) =>
       !players.contains(player) ==> {
-        val state = GameState(ListMap.empty ++ players.toList.map(n => n -> 0))
+        val state = GameState(ListMap.empty ++ players.toList.map(n => n -> Position(0)))
         val changedState = GameEngine.addPlayer(player, state)
 
         (changedState.players.keySet mustEqual players + player) and
@@ -25,14 +30,42 @@ class HelloSpec extends Specification with ScalaCheck {
       }
     }
 
-  def e2 =
+  private def addPlayer2 =
     prop { players: List[String] =>
       players.nonEmpty ==> {
-        val state = GameState(ListMap.empty ++ players.map(n => n -> 0))
+        val state = GameState(ListMap.empty ++ players.map(n => n -> Position(0)))
         val changedState = GameEngine.addPlayer(players.head, state)
 
         (changedState.players.keySet mustEqual players.toSet) and
           (changedState.message mustEqual s"${players.head}: already existing player")
       }
     }
+
+  private def movePlayer1 =
+    prop { (player: String, startingPosition: Int, diceRollOne: Int, diceRollTwo: Int) =>
+      (player.nonEmpty && startingPosition + diceRollOne + diceRollTwo < Int.MaxValue) ==> {
+        val state = GameState(ListMap.empty + (player -> Position(startingPosition)))
+        val changedState = GameEngine.movePlayer(player, diceRollOne, diceRollTwo, state)
+
+        (changedState.players(player).value mustEqual startingPosition + diceRollOne + diceRollTwo) and
+          (changedState.message mustEqual s"$player rolls $diceRollOne, $diceRollTwo. $player moves " +
+            s"from ${state.players(player).asString} to ${changedState.players(player).asString}")
+      }
+    }
+
+  private def movePlayerSpec = {
+    val pippo = "Pippo"
+    val pluto = "Pluto"
+    val state = GameState(ListMap.empty ++ List(pippo -> Position(0), pluto -> Position(0)))
+
+    val state1 = GameEngine.processInput(state, { () => "move Pippo 4, 2" })
+    state1.message mustEqual "Pippo rolls 4, 2. Pippo moves from Start to 6"
+
+    val state2 = GameEngine.processInput(state1, { () => "move Pluto 2, 2" })
+    state2.message mustEqual "Pluto rolls 2, 2. Pluto moves from Start to 4"
+
+    val state3 = GameEngine.processInput(state2, { () => "move Pippo 2, 3" })
+    state3.message mustEqual "Pippo rolls 2, 3. Pippo moves from 6 to 11"
+
+  }
 }
